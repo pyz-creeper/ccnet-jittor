fix small bug in eval(use np.int32 instead of np.uint8 to avoid overflow problem)

evaluate/test.py
@@ -1,13 +1,10 @@
 import jittor
 from datasets.dataloaders import ADE20k
-from networks.resnet import Resnet101
-from networks.ccnet import CCHead
 import numpy as np
+from tqdm import tqdm
 
 def get_confusion_matrix(gt_label, pred_label, class_num):
-    gt_label = gt_label.reshape(-1)
-    pred_label = pred_label.reshape(-1)
-    index = (gt_label * class_num + pred_label).astype('int32')
+    index = (gt_label * class_num + pred_label).astype('int32').reshape(-1)
     label_count = np.bincount(index)
     confusion_matrix = np.zeros((class_num, class_num))
 
@@ -16,7 +13,6 @@ def get_confusion_matrix(gt_label, pred_label, class_num):
             cur_index = i_label * class_num + i_pred_label
             if cur_index < len(label_count):
                 confusion_matrix[i_label, i_pred_label] = label_count[cur_index]
-
     return confusion_matrix
 
 def test_single_gpu(model,class_num=150):
@@ -24,10 +20,10 @@ def test_single_gpu(model,class_num=150):
     dataset = ADE20k(1,"./ADEChallengeData2016",train=False) # load val set!
     confusion_matrix = np.zeros((class_num,class_num))
     with jittor.no_grad():
-        for index, (img, ann) in enumerate(dataset):
-            ann = ann.numpy().astype(np.uint8)
+        for index, (img, ann) in tqdm(enumerate(dataset)):
+            ann = ann.numpy().astype(np.int32)
             output = model(img)
-            seg_pred = np.asarray(np.argmax(output.numpy(), axis=1), dtype=np.uint8)
+            seg_pred = np.asarray(np.argmax(output.numpy(), axis=1), dtype=np.int32)
             confusion_matrix += get_confusion_matrix(ann, seg_pred, class_num)
         pos = confusion_matrix.sum(1)
         res = confusion_matrix.sum(0)
@@ -38,10 +34,17 @@ def test_single_gpu(model,class_num=150):
         print("mean_IoU:",mean_IU)
 
 
-if __name__ == "__main__":
-    model = Resnet101()
-    head = CCHead()
-    x = jittor.random([10,3,512,683])
-    x = model(x)
-    out = head(x)
-    print(out.shape)
+# if __name__ == "__main__":
+#     dataset = ADE20k(1,"../ADEChallengeData2016",train=False)
+#     confusion_matrix = np.zeros((150,150))
+#     with jittor.no_grad():
+#         for index, (img, ann) in tqdm(enumerate(dataset)):
+#             confusion_matrix += get_confusion_matrix(ann, ann, 150)
+#             break
+#         pos = confusion_matrix.sum(1)
+#         res = confusion_matrix.sum(0)
+#         tp = np.diag(confusion_matrix)
+
+#         IU_array = (tp / np.maximum(1.0, pos + res - tp))
+#         mean_IU = IU_array.mean()
+#         print("mean_IoU:",mean_IU)

test.py
@@ -1,27 +1,32 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
 import jittor
+jittor.flags.use_cuda = 1
 from datasets.dataloaders import ADE20k
 from networks.resnet import Resnet101
-from networks.ccnet import CCHead
+from networks.ccnet import CCnet
+from evaluate.test import test_single_gpu,get_confusion_matrix
+from tqdm import tqdm
+import numpy as np
 
-
-
-def test_single_gpu(model):
-    # TODO inference each result on model (result in what form?)
-    # TODO reshape the result so that it has the same shape with ann
-    # TODO cal mIoU
-    dataset = ADE20k(16,"../ADEChallengeData2016",train=False) # load val set!
-    results = []
-    for index, (img, ann) in enumerate(dataset):
-        res = model(img)
-        results.append(res)
-    dataset.evaluate(results)
-    pass
 
 
 if __name__ == "__main__":
-    model = Resnet101()
-    head = CCHead()
-    x = jittor.random([10,3,512,683])
-    x = model(x)
-    out = head(x)
-    print(out.shape)
+    model = CCnet()
+    model.load("./saves/ccnet_resnet_epoch1000.pkl")
+    test_single_gpu(model)
+    # dataset = ADE20k(1,"./ADEChallengeData2016",train=False)
+    # confusion_matrix = np.zeros((151,151))
+    # with jittor.no_grad():
+    #     for index, (img, ann) in tqdm(enumerate(dataset)):
+    #         ann = ann.numpy()
+    #         confusion_matrix += get_confusion_matrix(ann, ann, 151)
+    #         break
+    #     pos = confusion_matrix.sum(1)
+    #     res = confusion_matrix.sum(0)
+    #     tp = np.diag(confusion_matrix)
+    #     IU_array = (tp / np.maximum(1.0, pos + res - tp))
+    #     mean_IU = IU_array.mean()
+    #     print("mean_IoU:",mean_IU)
+

train.py
@@ -1,5 +1,5 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1,2'
 
 from networks.ccnet import CCnet
 from datasets.dataloaders import ADE20k
@@ -13,15 +13,14 @@ from tqdm import tqdm
 import time
 
 learning_rate = 1e-2
-global_step = 80000
+global_step = 10000
 eval_gap = 800
-save_every = 8000
+save_every = 1000
 
 def train():
     model = CCnet()
     model.train()
-    dataset = ADE20k(1,"./ADEChallengeData2016",train=True,shuffle=True)
-    eval_set = ADE20k(1,"./ADEChallengeData2016",train=False)
+    dataset = ADE20k(1,"./ADEChallengeData2016",train=False,shuffle=True)
     criterion = CriterionDSN()
     optimizer = nn.SGD(model.parameters(),learning_rate,0.9,0.0005)
     step = 0
@@ -31,15 +30,15 @@ def train():
             out = model(img)
             loss = criterion([out],ann)
             optimizer.lr = learning_rate * ((1-step/global_step)**0.9)
+            print(loss)
             optimizer.step(loss)
             optimizer.zero_grad()
             step += 1
-            if step%eval_gap == 0:
-                test_single_gpu(model)
-                exit(1)
-                model.train()
+            # if step%eval_gap == 0:
+            #     test_single_gpu(model)
+            #     model.train()
             if step%save_every == 0:
-                model.save("./saves/ccnet_resnet_epoch%d.pth"%(step))
+                model.save("./saves/ccnet_resnet_epoch%d.pkl"%(step))
 
 
 if __name__ == "__main__":