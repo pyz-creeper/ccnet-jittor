54282014ebd8af1dfa62577a86e3ac45fbaf15d2
lzy-tony <liuzeyu1016@126.com>
2022-12-01 22:11:38 +0800

    feat: add neighborhood and dilated cc attention block, fix train to larger batch

--- networks/cc_attention.py
+++ networks/cc_attention.py
@@ -56,3 +56,82 @@ class CrissCrossAttention(nn.Module):
         s = self.__class__.__name__
         s += f'(in_channels={self.in_channels})'
         return s
+
+
+class DilatedCrissCrossAttention(nn.Module):
+    def __init__(self, in_channels: int, dilated: int) -> None:
+        super().__init__()
+        self.query_conv = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.key_conv = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.value_conv = nn.Conv2d(in_channels, in_channels, 1)
+        self.gamma = Scale(0.)
+        self.in_channels = in_channels
+        self.dilated = dilated
+
+    def execute(self,x):
+        B, C, H, W = x.shape
+
+        # calculate dilated mask
+        h_mask = jt.full_like(x, jt.Var(float('-inf')))
+        h_mask = h_mask.reindex(h_mask.shape, ['i0', 'i1', 'i2', 'i3'], overflow_conditions=[f'i3%{self.dilated}==i1%{self.dilated}'], overflow_value=0) # dilated
+        w_mask = jt.full_like(x, jt.Var(float('-inf')))
+        w_mask = w_mask.reindex(w_mask.shape, ['i0', 'i1', 'i2', 'i3'], overflow_conditions=[f'i3%{self.dilated}==i2%{self.dilated}'], overflow_value=0) # dilated
+
+        query = self.query_conv(x)
+        key = self.key_conv(x)
+        value = self.value_conv(x)
+        energy_H = jt.linalg.einsum('bchw,bciw->bwhi', query, key) + NEG_INF_DIAG(H) + h_mask
+        energy_H = energy_H.transpose(1, 2)
+        energy_W = jt.linalg.einsum('bchw,bchj->bhwj', query, key) + w_mask
+        attn = jt.nn.softmax(
+            jt.concat([energy_H, energy_W], dim=-1), dim=-1)  # [B,H,W,(H+W)]
+        out = jt.linalg.einsum('bciw,bhwi->bchw', value, attn[..., :H])
+        out += jt.linalg.einsum('bchj,bhwj->bchw', value, attn[..., H:])
+
+        out = self.gamma(out) + x
+        return out
+
+    def __repr__(self) -> str:
+        s = self.__class__.__name__
+        s += f'(in_channels={self.in_channels})'
+        return s
+
+
+class NeighborhoodCrissCrossAttention(nn.Module):
+    def __init__(self, in_channels: int) -> None:
+        super().__init__()
+        self.query_conv = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.key_conv = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.value_conv = nn.Conv2d(in_channels, in_channels, 1)
+        self.gamma = Scale(0.)
+        self.in_channels = in_channels
+
+    def execute(self,x):
+        B, C, H, W = x.shape
+        quarter_H = H // 4
+        quarter_W = W // 4
+
+        # calculate dilated mask
+        h_mask = jt.full_like(x, jt.Var(float('-inf')))
+        h_mask = h_mask.reindex(h_mask.shape, ['i0', 'i1', 'i2', 'i3'], overflow_conditions=[f'((i3-i1)%{H}+{H})%{H}<{quarter_H}', f'((i1-i3)%{H}+{H})%{H}<{quarter_H}'], overflow_value=0) # neighborhood
+        w_mask = jt.full_like(x, jt.Var(float('-inf')))
+        w_mask = w_mask.reindex(w_mask.shape, ['i0', 'i1', 'i2', 'i3'], overflow_conditions=[f'((i3-i2)%{W}+{W})%{W}<{quarter_W}', f'((i2-i3)%{W}+{W})%{W}<{quarter_W}'], overflow_value=0) # neighborhood
+
+        query = self.query_conv(x)
+        key = self.key_conv(x)
+        value = self.value_conv(x)
+        energy_H = jt.linalg.einsum('bchw,bciw->bwhi', query, key) + NEG_INF_DIAG(H) + h_mask
+        energy_H = energy_H.transpose(1, 2)
+        energy_W = jt.linalg.einsum('bchw,bchj->bhwj', query, key) + w_mask
+        attn = jt.nn.softmax(
+            jt.concat([energy_H, energy_W], dim=-1), dim=-1)  # [B,H,W,(H+W)]
+        out = jt.linalg.einsum('bciw,bhwi->bchw', value, attn[..., :H])
+        out += jt.linalg.einsum('bchj,bhwj->bchw', value, attn[..., H:])
+
+        out = self.gamma(out) + x
+        return out
+
+    def __repr__(self) -> str:
+        s = self.__class__.__name__
+        s += f'(in_channels={self.in_channels})'
+        return s
