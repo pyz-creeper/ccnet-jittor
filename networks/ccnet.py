b94774d070f612459f5be4092117570280525a75
pyz-creeper <pangyuanzhe2002@163.com>
2022-12-02 10:32:16 +0800

    add auxiliary head for training

--- networks/ccnet.py
+++ networks/ccnet.py
@@ -46,23 +46,34 @@ class CCHead(nn.Module):
 # TODO implement the auxiliary head!
 # Not very necessary though
 
-class AuxHead(nn.Module):
-    def __init__(self) -> None:
-        pass
+class AuxiliaryAttentionHead(nn.Module):
+    def __init__(self, in_index=-2, in_channels=1024, channels=256, dropout_rate=0.1, num_classes=151):
+        self.in_index = in_index
+        self.channels = channels
+        self.convs = ConvModule(in_channel=in_channels,out_channel=channels,kernel_size=3,padding=1,dilation=1)
+        self.dropout = Dropout2d(p=dropout_rate)
+        self.conv_seg = nn.Conv(channels,num_classes,kernel_size=1)
+    
+    def execute(self, inputs):
+        x = inputs[self.in_index]
+        feat = self.convs(x)
+        feat = self.dropout(feat)
+        output = self.conv_seg(feat)
+        return output
 
-    def execute(self):
-        pass
 
 class CCnet(nn.Module):
     def __init__(self) -> None:
         self.backbone = Resnet101(pretrained=True)
         self.decoder = CCHead()
-        # self.aux_decoder = AuxHead()
+        self.aux_decoder = AuxiliaryAttentionHead()
 
     def execute(self,x):
         output_features = self.backbone(x)
-        output = self.decoder(output_features)
-        output = nn.resize(output,x.shape[2:],mode="bilinear")
-        return output,output_features[-2]
+        output_main = self.decoder(output_features)
+        output_main = nn.resize(output_main,x.shape[2:],mode="bilinear")
+        output_aux = self.aux_decoder(output_features)
+        output_aux = nn.resize(output_aux,x.shape[2:],mode="bilinear")
+        return output_main,output_aux
         
         
